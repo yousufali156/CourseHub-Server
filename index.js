d04fb50224583ee33b8e169d42b3b184fcc52a3d
yousufali156 <yousufwebwork@gmail.com>
2025-06-17 12:58:43 -0500

    ok

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/jwt', async (req, res) => {
         res.cookie('token', customJwt, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
